f60a2f3e2f4bc1bbee07d11a8bca0fe4686fdd09
Peter N <peterNelson1@proton.me>
2023-05-01 17:22:22 -0700

    improved movement prefab
    movement prefab
    particle prototype

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -6,7 +6,11 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 6f;
-
+    
+    public float jumpAmount = 10;
+    public float jumpForce=20;
+    public float gravity = -9.81f;
+    float velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +22,32 @@ public class ThirdPersonMovement : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;
+        //float vertical = Input.GetAxisRaw("Vertical");
+        //Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;
+
+        velocity += gravity * Time.deltaTime;
+        if(transform.position.y < 0.1 && velocity < 0) 
+        {
+            velocity = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity = jumpForce;
+        }  
+
+        Vector3 direction = new Vector3(horizontal, velocity, 0.0f).normalized;
 
         if(direction.magnitude >= 0.1f)
         {
             controller.Move(direction * speed * Time.deltaTime);
+        }     
+
+
+        if(Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("Firing");
         }
+
     }
 }
+
